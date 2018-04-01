787b797168a2a3e4c3b964a13deefe4a847632f0
Trịnh Văn Minh <trinhvanminh2009@gmail.com>
2018-04-01 15:18:18 +0700

    Update Base part 1

---
+++ app/src/main/java/com/example/minh/quickbrain/Base/BasePresenter.java
@@ -0,0 +1,9 @@
+package com.example.minh.quickbrain.Base;
+
+/**
+ * Created by Minh on 4/1/2018.
+ */
+
+public interface BasePresenter {
+    void start();
+}
---
+++ app/src/main/java/com/example/minh/quickbrain/Base/BaseView.java
@@ -0,0 +1,9 @@
+package com.example.minh.quickbrain.Base;
+
+/**
+ * Created by Minh on 4/1/2018.
+ */
+
+public interface BaseView<T> {
+    void setPresenter(T presenter);
+}
